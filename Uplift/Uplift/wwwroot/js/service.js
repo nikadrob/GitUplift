9588995fe744f1877d13b727c0f11d96baca3838
snasteski <nikadrob@live.com>
2019-11-21 14:09:37 +0100

    BindProperty

--- Uplift/Uplift/wwwroot/js/service.js
+++ Uplift/Uplift/wwwroot/js/service.js
@@ -14,9 +14,9 @@ function loadDataTable() {
         },
         "columns": [
             { "data": "name", "width": "20%" },
-            { "data": "categoryId", "width": "20%" },
+            { "data": "category.name", "width": "20%" },
             { "data": "price", "width": "15%" },
-            { "data": "frequencyId", "width": "15%" },
+            { "data": "frequency.frequencyCount", "width": "15%" },
             {
                 "data": "id",
                 "render": function (data) {
